770fa7fc753d4bf4cb977abe49d2e7c81cb0a378
Abdulaziz Raxmonaliyev <rahmonaliyev2007@icloud.com>
2025-07-27 13:24:18 +0500

    images fixed

--- app/modules/Projects/Projects.tsx
+++ app/modules/Projects/Projects.tsx
@@ -1,5 +1,4 @@
 import Heading from '@/app/components/Heading/Heading'
-import { AquaSite, B2BSite } from '@/public/assets/images/svgImages'
 import React from 'react'
 
 const Projects = () => {
@@ -8,10 +7,10 @@ const Projects = () => {
       <Heading text='Проекты' title={<>Реализованные решения, <br /> подтверждённые практикой.</>} paragraph='В этом разделе представлены реальные кейсы и проекты, которые мы успешно реализовали для клиентов из разных сфер. Каждый из них — результат глубокого анализа, индивидуального подхода и технологической экспертизы. Мы гордимся тем, что наши цифровые продукты помогают бизнесам расти, упрощать процессы и двигаться вперёд.' />
       <div className='flex justify-between items-center gap-[33px]'>
         <div className='w-[50%] bg-gray-400 flex justify-center items-center !px-[65px] !pt-[60px] !pb-[30px]'>
-        <B2BSite />
+       
       </div>
       <div className='w-[50%] bg-gray-400 flex justify-center items-center !px-[65px] !pt-[60px] !pb-[30px]'>
-        <AquaSite />
+        
       </div>
       </div>
     </div>
