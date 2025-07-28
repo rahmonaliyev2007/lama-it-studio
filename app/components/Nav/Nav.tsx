1846fcf457aa7d95f5dce52a00cd6f640467a083
Abdulaziz Raxmonaliyev <rahmonaliyev2007@icloud.com>
2025-07-28 13:18:58 +0500

    nav fixed

--- app/components/Nav/Nav.tsx
+++ app/components/Nav/Nav.tsx
@@ -26,40 +26,20 @@ const Nav = () => {
     const left = rect.left - containerRect.left;
     const top = rect.top - containerRect.top;
 
-    setIndicatorStyle({
-      width: `${width}px`,
-      height: `${height}px`,
-      transform: `translate(${left + 5}px, ${top}px)`,
-    });
+    setIndicatorStyle({ width: `${width}px`, height: `${height}px`, transform: `translate(${left + 5}px, ${top}px)`});
   };
 
   return (
     <div className="relative inline-block" ref={containerRef}>
-      <ul
-        onMouseEnter={() => setIsMouseThere(true)}
-        onMouseLeave={() => setIsMouseThere(false)}
-        className="flex justify-center items-center border border-[#353535] p-2 rounded-[15px] overflow-hidden bg-[#0F0F0FCC] relative"
-      >
+      <ul onMouseEnter={() => setIsMouseThere(true)} onMouseLeave={() => setIsMouseThere(false)} className="flex justify-center items-center border border-[#353535] p-2 rounded-[15px] overflow-hidden bg-[#0F0F0FCC] relative">
         {navItems.map((item, idx) => (
-          <li
-            key={idx}
-            onMouseEnter={handleMouseEnter}
-            className="relative z-10 !px-[22px] !py-[10px] lowercase font-medium bg-[#0F0F0FCC]"
-          >
-            <a
-              href={item.link}
-              className="text-[16px] text-[#777777] hover:text-[#6BB0FF] duration-1000 cursor-pointer block"
-            >
+          <li key={idx} onMouseEnter={handleMouseEnter} className="relative z-10 !px-[22px] !py-[10px] lowercase font-medium bg-[#0F0F0FCC]">
+            <a href={item.link} className="text-[16px] text-[#777777] hover:text-[#6BB0FF] duration-1000 cursor-pointer block" >
               {item.title}
             </a>
           </li>
         ))}
-        <span
-          className={`nav-indicator ${
-            isMouseThere ? "opacity-100 duration-300" : "opacity-0"
-          }`}
-          style={indicatorStyle}
-        ></span>
+        <span className={`nav-indicator ${ isMouseThere ? "opacity-100 duration-300" : "opacity-0" }`} style={indicatorStyle} ></span>
       </ul>
     </div>
   );
